4b096a5cdb964c36830ceccda79a50d844bebee8
tushar swami <you@example.com>
2024-11-18 14:57:35 +0530

    some change

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 
-const status= require('../controllers/status')
-
-router.use('/doctors', require('./doctors'))
-router.use('/patients', require('./patients'))
-router.get('/reports/:status',passport.authenticate('jwt',{session:false}),status.filteredReports)
-
+const status = require("../controllers/status");
+router.get("/", (req, res) => {
+  return res.status(200).json({ message: "now live" });
+});
+router.use("/doctors", require("./doctors"));
+router.use("/patients", require("./patients"));
+router.get(
+  "/reports/:status",
+  passport.authenticate("jwt", { session: false }),
+  status.filteredReports
+);
 
 module.exports = router;
